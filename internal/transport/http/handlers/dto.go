ecaa6b18a406a11f32185695793ca74d172f78ef
Barba10s <kosta9019@mail.ru>
2026-04-08 16:29:04 +0300

    #6 #add #dto.go - add frequency to dto

--- internal/transport/http/handlers/dto.go
+++ internal/transport/http/handlers/dto.go
@@ -4,12 +4,14 @@ import (
 	"time"
 
 	taskdomain "example.com/taskservice/internal/domain/task"
+	taskfrequency "example.com/taskservice/internal/domain/task/frequency"
 )
 
 type taskMutationDTO struct {
 	Title       string            `json:"title"`
 	Description string            `json:"description"`
 	Status      taskdomain.Status `json:"status"`
+	Frequency   *taskfrequency.Frequency    `json:"frequency,omitempty"`
 }
 
 type taskDTO struct {
@@ -17,6 +19,7 @@ type taskDTO struct {
 	Title       string            `json:"title"`
 	Description string            `json:"description"`
 	Status      taskdomain.Status `json:"status"`
+	Frequency   *taskfrequency.Frequency    `json:"frequency,omitempty"`
 	CreatedAt   time.Time         `json:"created_at"`
 	UpdatedAt   time.Time         `json:"updated_at"`
 }
@@ -27,6 +30,7 @@ func newTaskDTO(task *taskdomain.Task) taskDTO {
 		Title:       task.Title,
 		Description: task.Description,
 		Status:      task.Status,
+		Frequency:   task.Frequency,
 		CreatedAt:   task.CreatedAt,
 		UpdatedAt:   task.UpdatedAt,
 	}
