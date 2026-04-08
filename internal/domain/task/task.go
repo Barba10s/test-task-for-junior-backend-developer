d8bfe97e6cba126cf0ae7f4fe7ab2fb5cb6ff038
Barba10s <kosta9019@mail.ru>
2026-04-08 15:25:25 +0300

    #2 #add #task.go - add frequency field

--- internal/domain/task/task.go
+++ internal/domain/task/task.go
@@ -1,6 +1,10 @@
 package task
 
-import "time"
+import (
+	"time"
+
+	"example.com/taskservice/internal/domain/task/frequency"
+)
 
 type Status string
 
@@ -11,12 +15,13 @@ const (
 )
 
 type Task struct {
-	ID          int64     `json:"id"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	Status      Status    `json:"status"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID          int64                `json:"id"`
+	Title       string               `json:"title"`
+	Description string               `json:"description"`
+	Status      Status               `json:"status"`
+	Frequency   *frequency.Frequency `json:"frequency,omitempty"`
+	CreatedAt   time.Time            `json:"created_at"`
+	UpdatedAt   time.Time            `json:"updated_at"`
 }
 
 func (s Status) Valid() bool {
