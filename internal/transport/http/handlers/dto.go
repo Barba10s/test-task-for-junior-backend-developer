c04089436e9a90dbea314e5bf538ebd70c3e66f5
Barba10s <kosta9019@mail.ru>
2026-04-09 14:18:32 +0300

    #6 #add #dto.go - return frequency description

--- internal/transport/http/handlers/dto.go
+++ internal/transport/http/handlers/dto.go
@@ -8,24 +8,25 @@ import (
 )
 
 type taskMutationDTO struct {
-	Title       string            `json:"title"`
-	Description string            `json:"description"`
-	Status      taskdomain.Status `json:"status"`
-	Frequency   *taskfrequency.Frequency    `json:"frequency,omitempty"`
+	Title       string                   `json:"title"`
+	Description string                   `json:"description"`
+	Status      taskdomain.Status        `json:"status"`
+	Frequency   *taskfrequency.Frequency `json:"frequency,omitempty"`
 }
 
 type taskDTO struct {
-	ID          int64             `json:"id"`
-	Title       string            `json:"title"`
-	Description string            `json:"description"`
-	Status      taskdomain.Status `json:"status"`
-	Frequency   *taskfrequency.Frequency    `json:"frequency,omitempty"`
-	CreatedAt   time.Time         `json:"created_at"`
-	UpdatedAt   time.Time         `json:"updated_at"`
+	ID                   int64                    `json:"id"`
+	Title                string                   `json:"title"`
+	Description          string                   `json:"description"`
+	Status               taskdomain.Status        `json:"status"`
+	Frequency            *taskfrequency.Frequency `json:"frequency,omitempty"`
+	FrequencyDescription string
+	CreatedAt            time.Time `json:"created_at"`
+	UpdatedAt            time.Time `json:"updated_at"`
 }
 
 func newTaskDTO(task *taskdomain.Task) taskDTO {
-	return taskDTO{
+	dto := taskDTO{
 		ID:          task.ID,
 		Title:       task.Title,
 		Description: task.Description,
@@ -34,4 +35,10 @@ func newTaskDTO(task *taskdomain.Task) taskDTO {
 		CreatedAt:   task.CreatedAt,
 		UpdatedAt:   task.UpdatedAt,
 	}
+
+	if task.Frequency != nil {
+		dto.FrequencyDescription = task.Frequency.Description()
+	}
+
+	return dto
 }
